25ef331859366152589cc5f39b57bf3c6575ba73
Simran Deol <simrandeol@Simrans-MacBook-Pro.local>
2020-03-26 19:40:03 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import Helloworld from "./HelloWorld";
-import { createStore } from "redux";
-import reducer from "./reducers/index";
-
-const initialState = { tech: "React" };
-const store = createStore(reducer, initialState);
+import { store } from "./store";
+import ButtonGroup from "./ButtonGroup";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Helloworld tech={store.getState().tech} />
+        <Helloworld key={1} tech={store.getState().tech} />
+        <ButtonGroup key={2} technologies={["React", "Elm", "React-redux"]} />
       </div>
     );
   }
---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+export const setTechnology = text => {
+  return {
+    type: "SET_TECHNOLOGY",
+    text: text
+  };
+};
